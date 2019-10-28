79de91b48f3d4bdbd6e3125c53af6fa0e837b846
juliagraves <gravehaus@gmail.com>
2019-10-28 10:03:09 -0700

    Updates

---
+++ precache-manifest.3b3f61e7a938a5f0bd8fd598c79de1a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78aa02f8adb7d0d9ae6a83c9f4d10ca8",
+    "url": "/SuperSmashMemory/index.html"
+  },
+  {
+    "revision": "90f1f3390f36accb8c75",
+    "url": "/SuperSmashMemory/static/css/main.2d5ccab6.chunk.css"
+  },
+  {
+    "revision": "f189f686c3cfc58dc824",
+    "url": "/SuperSmashMemory/static/js/2.bd3a9e65.chunk.js"
+  },
+  {
+    "revision": "90f1f3390f36accb8c75",
+    "url": "/SuperSmashMemory/static/js/main.a80adab1.chunk.js"
+  },
+  {
+    "revision": "6d0cdf15111ed506cf4c",
+    "url": "/SuperSmashMemory/static/js/runtime-main.8b04c26b.js"
+  }
+]);
--- src/components/wrapper/wrapper.js
+++
@@ -1,6 +0,0 @@
-  
-import React from "react";
-
-const wrapper = props => <div className="wrapper">{props.children}</div>;
-
-export default wrapper;
